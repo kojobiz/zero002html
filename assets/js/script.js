9d084290c7f501445cb3ed7e9ccd586790947447
小城 <kojolumi@gamil.com>
2024-07-02 19:26:41 +0900

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,15 @@
 document.getElementById('spnav-open').addEventListener('click', function() {
     document.querySelector('.l-spnav').style.display = 'block';
 });
+document.getElementById('spnav-open').addEventListener('click', function() {
+    document.querySelector('.l-spnav').style.display = 'block';
+});
 
 // sp-close
 document.getElementById('spnav-close').addEventListener('click', function() {
     document.querySelector('.l-spnav').style.display = 'none';
 });
-// document.querySelectorAll('.p-spnav-listbox__title').forEach(function(item) {
-//     item.addEventListener('click', function() {
-//         document.querySelector('.l-spnav').style.display = 'none';
-//     });
-// });
+
 
 
 // 遷移時にオフセットと非表示
