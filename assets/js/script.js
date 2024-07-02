d5d106ff8f7f6a8899b2dcbba4fa572fb026227d
小城 <kojolumi@gamil.com>
2024-07-02 18:48:08 +0900

    html line fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,25 @@ document.getElementById('spnav-close').addEventListener('click', function() {
 
 
 // 遷移時にオフセットと非表示
+// pc
+document.querySelectorAll('.p-nav-listbox__title').forEach(function(item) {
+    item.addEventListener('click', function(event) {
+        event.preventDefault(); // デフォルトのアンカー動作を防ぐ
+        const targetId = this.querySelector('a').getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId); 
+
+        if (targetElement) {
+            const offsetTop = targetElement.getBoundingClientRect().top + window.scrollY - 117; // 117pxオフセット
+            window.scrollTo({
+                top: offsetTop,
+                behavior: 'smooth' 
+            });
+        }
+        document.querySelector('.l-spnav').style.display = 'none';
+    });
+});
+
+// sp
 document.querySelectorAll('.p-spnav-listbox__title').forEach(function(item) {
     item.addEventListener('click', function(event) {
         event.preventDefault(); // デフォルトのアンカー動作を防ぐ
