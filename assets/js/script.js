752138f77d583783c8ac178d7f6c2d22a0aa5327
小城 <kojolumi@gamil.com>
2024-07-02 18:13:27 +0900

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,27 @@ document.getElementById('spnav-open').addEventListener('click', function() {
 document.getElementById('spnav-close').addEventListener('click', function() {
     document.querySelector('.l-spnav').style.display = 'none';
 });
+// document.querySelectorAll('.p-spnav-listbox__title').forEach(function(item) {
+//     item.addEventListener('click', function() {
+//         document.querySelector('.l-spnav').style.display = 'none';
+//     });
+// });
+
+
+// 遷移時にオフセットと非表示
 document.querySelectorAll('.p-spnav-listbox__title').forEach(function(item) {
-    item.addEventListener('click', function() {
+    item.addEventListener('click', function(event) {
+        event.preventDefault(); // デフォルトのアンカー動作を防ぐ
+        const targetId = this.querySelector('a').getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId); 
+
+        if (targetElement) {
+            const offsetTop = targetElement.getBoundingClientRect().top + window.scrollY - 70; // 70pxオフセット
+            window.scrollTo({
+                top: offsetTop,
+                behavior: 'smooth' 
+            });
+        }
         document.querySelector('.l-spnav').style.display = 'none';
     });
 });
